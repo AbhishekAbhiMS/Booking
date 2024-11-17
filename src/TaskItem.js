68375caa10e2f967c11f05d791116b0da8cca2e0
AbhishekAbhiMS <128393773+AbhishekAbhiMS@users.noreply.github.com>
2024-11-17 17:20:56 +0530

    TaskItem.js

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+const TaskItem = ({ task, deleteTask, toggleTaskCompletion }) => {
+  return (
+    <div className={`task-item ${task.completed ? "completed" : ""}`}>
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={() => toggleTaskCompletion(task.id)}
+      />
+      <span className={`task-title priority-${task.priority.toLowerCase()}`}>
+        {task.title} ({task.priority})
+      </span>
+      <button onClick={() => deleteTask(task.id)}>Delete</button>
+    </div>
+  );
+};
+
+export default TaskItem;
